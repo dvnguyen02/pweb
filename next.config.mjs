5f988545557d432bcdb94cdb2e92950191915a2c
dvnguyen02 <duynguyen290502@gmail.com>
2025-05-23 02:20:48 +1200

    ready to deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  output: 'export',
+  output: isProd ? 'export' : undefined,
   reactStrictMode: true,
+  trailingSlash: true,
   images: {
     domains: ['i.gyazo.com'],
     unoptimized: true,
   },
+  assetPrefix: isProd ? '/pweb/' : '',
+  basePath: isProd ? '/pweb' : '',
   env: {
     PROXYCURL_API_KEY: process.env.PROXYCURL_API_KEY,
     GITHUB_TOKEN: process.env.GITHUB_TOKEN,
