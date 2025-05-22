2308c8c00a8035dca8c60803b75bd06667fa82ba
dvnguyen02 <duynguyen290502@gmail.com>
2025-05-23 02:55:22 +1200

    okay again..

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,12 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  // output: isProd ? 'export' : undefined, // REMOVE or COMMENT THIS LINE
   reactStrictMode: true,
   trailingSlash: true,
   images: {
     domains: ['i.gyazo.com'],
     unoptimized: true,
   },
-  assetPrefix: isProd ? '' : '',
-  basePath: isProd ? '' : '',
   env: {
     PROXYCURL_API_KEY: process.env.PROXYCURL_API_KEY,
     GITHUB_TOKEN: process.env.GITHUB_TOKEN,
